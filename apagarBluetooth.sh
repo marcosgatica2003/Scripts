4c83fa3eda862bd8297584f536a8916498953bbf
marcosgatica2003 <marcosgatica1112@gmail.com>
2024-10-16 08:12:37 -0300

    Borrado de apagarBluetooth.sh & creación del apagarSistema.sh

--- apagarBluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Apagando el servicio de Bluetooth..."
-sudo systemctl stop bluetooth.service
-sudo systemctl mask bluetooth.service
-echo "Servicio de Bluetooth apagado y enmascarado."
-
